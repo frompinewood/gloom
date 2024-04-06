197384085fe440f79303c51acd44c98a9e5d3050
Micaiah Parker <frompinewood@icloud.com>
2024-04-06 16:06:04 -0400

    add options case to gloom:prompt

--- src/gloom_protocol.erl
+++ src/gloom_protocol.erl
@@ -20,10 +20,8 @@ handle_cast({send, Data}, {T, S, _, _} = State) ->
 handle_cast({prompt, Name, Prompts}, {T, S, H, P} = State) ->
     T:setopts(S, [{active, false}]),
     Response = lists:foldl(
-        fun({Tag, Prompt}, Acc) ->
-            T:send(S, Prompt),
-            {ok, Data} = T:recv(S, 0, 60000),
-            Acc#{Tag => string:trim(Data)}
+        fun(Prompt, Acc) ->
+            maps:merge(Acc, prompt(Prompt, T, S))
         end,
         #{},
         Prompts
@@ -42,3 +40,27 @@ handle_info({tcp, S, Data}, {_, S, H, P} = State) ->
 terminate(normal, {_, _, _, P}) ->
     lager:info("Stopped ~p.", [P]),
     ok.
+
+prompt({Tag, Prompt}, Transport, Socket) ->
+    Transport:send(Socket, Prompt),
+    case Transport:recv(Socket, 0, 60000) of
+        {ok, Data} -> #{Tag => string:trim(Data)};
+        {error, _} -> prompt({Tag, Prompt}, Transport, Socket)
+    end;
+prompt({Tag, Prompt, Options}, Transport, Socket) ->
+    Transport:send(Socket, [Prompt, "\n", string:join(Options, "\n"), "\n"]),
+    case Transport:recv(Socket, 0, 60000) of
+        {ok, Data} ->
+            Data1 = string:trim(Data),
+            case
+                lists:member(
+                    string:lowercase(Data1),
+                    lists:map(fun string:lowercase/1, Options)
+                )
+            of
+                true -> #{Tag => Data1};
+                false -> prompt({Tag, Prompt, Options}, Transport, Socket)
+            end;
+        {error, _} ->
+            prompt({Tag, Prompt, Options}, Transport, Socket)
+    end.
