48525c1d2c830c148b04b569ca9cc5fc972c282d
Micaiah Parker <frompinewood@icloud.com>
2024-04-08 08:30:42 -0400

    enhanced prompt option handling

--- src/gloom_protocol.erl
+++ src/gloom_protocol.erl
@@ -21,28 +21,23 @@ handle_cast({send, Data}, {T, S, _, _} = State) ->
     T:send(S, [Data, $\n]),
     {noreply, State};
 handle_cast({prompt, Name, Prompts}, {T, S, H, P} = State) ->
-    T:setopts(S, [{active, false}]),
-    Response = lists:foldl(
+    Fun =
         fun(Prompt, Acc) ->
             maps:merge(Acc, prompt(Prompt, T, S))
         end,
-        #{},
-        Prompts
-    ),
+    T:setopts(S, [{active, false}]),
+    Response = lists:foldl(Fun, #{}, Prompts),
     H:send(P, {Name, Response}),
     T:setopts(S, [{active, true}]),
     {noreply, State}.
 
 handle_info({tcp_closed, S}, {_, S, _, _} = State) ->
-    lager:info("~p closed.", [S]),
     {stop, normal, State};
 handle_info({tcp, S, Data}, {_, S, H, P} = State) ->
     H:send(P, string:trim(Data)),
     {noreply, State}.
 
-terminate(normal, {_, _, _, P}) ->
-    lager:info("Stopped ~p.", [P]),
-    ok.
+terminate(normal, _) -> ok.
 
 prompt({Tag, Prompt}, Transport, Socket) ->
     Transport:send(Socket, Prompt),
@@ -51,19 +46,46 @@ prompt({Tag, Prompt}, Transport, Socket) ->
         {error, _} -> prompt({Tag, Prompt}, Transport, Socket)
     end;
 prompt({Tag, Prompt, Options}, Transport, Socket) ->
-    Transport:send(Socket, [string:join(Options, "\n"),"\n", Prompt]),
-    case Transport:recv(Socket, 0, 60000) of
-        {ok, Data} ->
-            Data1 = string:trim(Data),
-            case
-                lists:member(
-                    binary_to_list(string:lowercase(Data1)),
-                    lists:map(fun string:lowercase/1, Options)
-                )
-            of
-                true -> #{Tag => Data1};
-                false -> prompt({Tag, Prompt, Options}, Transport, Socket)
-            end;
-        {error, _} ->
-            prompt({Tag, Prompt, Options}, Transport, Socket)
+    OptionText = format_options(Options),
+    Transport:send(Socket, [$\n, OptionText, Prompt]),
+    Choice =
+        case Transport:recv(Socket, 0, 60000) of
+            {ok, Data} -> validate_choice(Options, string:trim(Data));
+            {error, timeout} -> exit(normal);
+            {error, _} -> prompt({Tag, Prompt, Options}, Transport, Socket)
+        end,
+    ValidChoice =
+        case Choice of
+            {error, _} ->
+                prompt({Tag, Prompt, Options}, Transport, Socket);
+            Value ->
+                Value
+        end,
+    #{Tag => ValidChoice}.
+
+format_options(Options) ->
+    lists:map(
+        fun({I, O}) ->
+            io_lib:format("~p ~s~n", [I, O])
+        end,
+        lists:enumerate(Options)
+    ).
+
+validate_choice(Options, Data) ->
+    case
+        lists:search(
+            fun(Option) ->
+                string:lowercase(Option) =:= string:lowercase(Data)
+            end,
+            lists:map(fun list_to_binary/1, Options)
+        )
+    of
+        {value, Value} ->
+            Value;
+        false ->
+            try
+                lists:nth(binary_to_integer(Data), Options)
+            catch
+                error:Reason:_ -> {error, Reason}
+            end
     end.
